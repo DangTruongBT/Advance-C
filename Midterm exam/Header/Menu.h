c0d5f0ca85359ab4a7f5ebe836290d16e4252a5e
DangTruongBT <103482832+DangTruongBT@users.noreply.github.com>
2024-04-07 23:46:03 +0700

    Add files via upload

---
+++ Midterm exam/Header/Menu.h
@@ -0,0 +1,21 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+/*
+* File: Menu.h
+* Author: Minh Phat
+* Date: 5/4/2024
+* Description: This file contains all the functions prototypes for program display library.
+*/
+#ifndef __MENU_H
+#define __MENU_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include "member.h"
+
+void writeData(Member member);
+void updateMemberInFile(char *searchKey, Member new_member);
+void Menu();
+void runProgram();
+#endif
